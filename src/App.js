b984b4b1bec5bddf907db99db80e47b9769f6226
Mark W <markwuu@gmail.com>
2021-04-28 15:27:13 -0700

    Refactored App and Pet component with JSX

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import { render } from "react-dom";
 import { Pet } from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Match",
-      animal: "Dog",
-      breed: "Border collie",
-    }),
-    React.createElement(Pet, {
-      name: "Xiaobai",
-      animal: "Dog",
-      breed: "Husky",
-    }),
-    React.createElement(Pet, {
-      name: "Chloe",
-      animal: "Dog",
-      breed: "Toy Poodle",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Match" animal="Dog" breed="Border Collie" />
+      <Pet name="Xiaobai" animal="Dog" breed="Husky" />
+      <Pet name="Chloe" animal="Dog" breed="Toy Poodle" />
+    </div>
+  );
 };
 
 render(React.createElement(App), document.getElementById("root"));
