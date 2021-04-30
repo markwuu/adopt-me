d4decfbcd33d416e185e3a1e9d311cfdb38cc35f
Mark W <markwuu@gmail.com>
2021-04-30 10:31:38 -0700

    Created dropdown hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import { Pet } from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Match" animal="Dog" breed="Border Collie" />
-      <Pet name="Xiaobai" animal="Dog" breed="Husky" />
-      <Pet name="Chloe" animal="Dog" breed="Toy Poodle" />
+      <SearchParams />
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropDown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropDown] = useDropdown("Breed", "", breeds);
+
+  return (
+    <div className="search-params">
+      <h1>{location}</h1>
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropDown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
